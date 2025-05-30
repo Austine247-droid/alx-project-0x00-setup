308dbd557ffbf62b0d28bfbf3464f0b6936e5a2d
Austine247-droid <ugberaeseoa@gmail.com>
2025-05-30 18:55:10 +0000

    updated files

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -1,16 +1,15 @@
-import Button from "@/components/Button";
 import Card from "@/components/Card";
-
+import Button from "@/components/Button";
 const Landing: React.FC = () => {
   return (
     <div>
       <h1 className=" text-xl font-extralight">Landing Page</h1>
       <Card />
-      <div className="flex flex-col gap-2 mt-4">
-        <Button title="Book now" styles="bg-blue-200 rounded-sm " />
-        <Button title="Sell now" styles="bg-yellow-200 rounded-md " />
-        <Button title="Explore" styles="bg-white-500 rounded-full " />
-        <Button title="Click me" styles="bg-white-500 rounded-full " />
+      <div className="flex gap-4 mt-4">
+        <Button title="Explore" styles="rounded-sm" />
+        <Button title="Buy me" styles="rounded-md" />
+        <Button title="Buy Now" styles="rounded-lg" />
+        <Button title="Click Me" styles="rounded-full" />
       </div>
     </div>
   );
