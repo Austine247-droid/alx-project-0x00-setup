8be4c714d04ab6f192c675065505fd90e8391d1a
Austine247-droid <ugberaeseoa@gmail.com>
2025-05-30 18:28:22 +0000

    updated files

--- alx-project-0x00/components/Pill.tsx
+++ alx-project-0x00/components/Pill.tsx
@@ -1,9 +1,17 @@
-const Pill: React.FC = () => {
+import { PillProps } from "@/interfaces";
+
+const Pill: React.FC<PillProps> = ({ title }) => {
   return (
-    <div className=" flex justify-center  items-center bg-[#F9F9F9] px-3 w-auto h-[27px] rounded-full">
-      <p className=" text-sm ">Title</p>
+    <div className=" flex justify-center items-center bg-white px-3 w-auto h-[27px] rounded-full text-blue-400">
+      <p className="text-sm">{title}</p>
     </div>
   );
 };
 
 export default Pill;
+
+// const Pill = ({ title }: PillProps) => {
+//   return <div>{title}</div>;
+// };
+
+// export default Pill;
